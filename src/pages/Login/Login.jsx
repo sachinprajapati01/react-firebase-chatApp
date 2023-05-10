c9861d215e18b687667dcb903059cd264f832672
sachinprajapati01 <sachinprajapati1412.sk@gmail.com>
2023-05-11 01:47:23 +0530

    change in name

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -62,7 +62,7 @@ export default function Login() {
     <React.Fragment>
       <CardContent>
         <Typography variant='h4' color='text.secondary' gutterBottom>
-          Penguins Chat
+          Utkarsh Chat
         </Typography>
         <Box width='100%'>
           <div className='google-btn' role={'button'} onClick={(e) => handleSignIn(e, 'google')}>
